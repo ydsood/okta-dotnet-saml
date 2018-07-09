ae2491d9d7570d3703758aa42a567d2258e65b44
Yadu <ydsood@gmail.com>
2018-07-09 16:16:14 -0400

    Cleaning up code, adding a section to appsettings.json for SAML configuration and X509 certificate installation location.

--- okta-dotnet-saml/Controllers/AccountController.cs
+++ okta-dotnet-saml/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens.Saml2;
 
 namespace okta_dotnet_saml.Controllers
@@ -12,7 +13,21 @@ namespace okta_dotnet_saml.Controllers
     [Route("account")]
     public class AccountController : Controller
     {
-        private const string REDIRECT_URL = "https://fast.okta.com/app/fasttechnology_oktadotnetsaml_1/exk1m1hhntCbsCO0H2p7/sso/saml";
+        private string REDIRECT_URL;
+        private string ISSUER_NAME;
+        private StoreName STORE_NAME;
+        private StoreLocation STORE_LOCATION;
+
+        public AccountController(IConfiguration configuration)
+        {
+            IConfigurationSection SAML_sec = configuration.GetSection("SAML");
+            REDIRECT_URL = SAML_sec.GetValue(typeof(string), "redirect_url").ToString();
+            IConfigurationSection X509_sec = SAML_sec.GetSection("X509");
+            STORE_NAME = (StoreName)X509_sec.GetValue(typeof(StoreName), "StoreName");
+            ISSUER_NAME = (string)X509_sec.GetValue(typeof(string), "IssuerName");
+            STORE_LOCATION = (StoreLocation)X509_sec.GetValue(typeof(StoreLocation), "StoreLocation");
+        }
+
 
         [HttpGet("login")]
         public IActionResult Login() => Redirect(RedirectUrl());
@@ -26,9 +41,9 @@ namespace okta_dotnet_saml.Controllers
         public IActionResult SamlACS([FromForm] string samlResponse)
         {
             Console.Write(samlResponse);
-            X509Store store = new X509Store(StoreName.TrustedPublisher ,StoreLocation.LocalMachine);
+            X509Store store = new X509Store(STORE_NAME, STORE_LOCATION);
             store.Open(OpenFlags.ReadOnly);
-            X509Certificate2Collection certCollection = store.Certificates.Find(X509FindType.FindByIssuerName, "okta", false);
+            X509Certificate2Collection certCollection = store.Certificates.Find(X509FindType.FindByIssuerName, ISSUER_NAME, false);
             X509Certificate2 oktaCert = certCollection[0];
             store.Close();
 
--- okta-dotnet-saml/Startup.cs
+++ okta-dotnet-saml/Startup.cs
@@ -23,6 +23,7 @@ namespace okta_dotnet_saml
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfiguration>(Configuration);
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie();
             services.AddMvc();
